338ce2ff443a0f3196b3117ff080b7109ef1b033
Demétrio Alves Atra <datra@verg1759.lobtec.com.br>
2022-07-07 10:23:16 -0300

    v. 1.1.0

--- src/App.js
+++ src/App.js
@@ -2,16 +2,66 @@ import { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+
+  constructor (props) {
+    super (props)
+    this.state = {
+      number: 0,
+      text: 'START'
+    }
+
+    // Atributos da classe
+    this.timer = null
+
+    // Bind de métodos
+    this.start = this.start.bind(this)
+    this.clear = this.clear.bind(this)
+  }
+
+  start () {
+    // Guarda a state
+    let state = this.state
+
+    if (this.timer == null) {
+      this.timer = setInterval( () => {
+        state.number += 0.1
+        this.setState(state)
+      }, 100)
+      state.text = 'PAUSE'
+    } else {
+      clearInterval(this.timer)
+      this.timer = null
+      state.text = 'START'
+    }
+
+    this.setState(state)
+  }
+
+  clear () {
+    // Guarda a state
+    let state = this.state
+
+    if (this.timer !== null) {
+      this.clearInterval(this.timer)
+      this.timer = null
+    }
+
+    state.number = 0
+    state.text = 'START'
+
+    this.setState(this.state)
+  }
+
   render () {
     return (
       <div className = 'App'>
 
         <img className = 'img' src = { require('./imgs/timer.png') }></img>
-        <a className = 'timer'>0.0</a>
+        <a className = 'timer'>{ this.state.number.toFixed(1) }</a>
 
         <div className = 'buttons'>
-          <a className = 'btn'>START</a>
-          <a className = 'btn'>CLEAR</a>
+          <a className = 'btn' onClick = { this.start }>{ this.state.text }</a>
+          <a className = 'btn' onClick = { this.clear }>CLEAR</a>
         </div>
 
       </div>
